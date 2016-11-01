6434d697dbf2f7d7163de69761085b077fc9cc65
Robin Broda <robin@broda.me>
2016-11-01 16:01:44 +0100

    Add helper methods

---
+++ bash_include/0_colors.sh
@@ -0,0 +1,3 @@
+export COL_NORM="$(tput setaf 9)"
+export COL_RED="$(tput setaf 1)"
+export COL_GREEN="$(tput setaf 2)"
--- bash_include/1_helpers.sh
+++ bash_include/1_helpers.sh
@@ -1,3 +1,21 @@
 function locate_package () {
   ! [ -z "$(whereis $1 | cut -d':' -f2-)" ]
 }
+
+function cdr_exedir () {
+	pushd `dirname $0` > /dev/null
+	echo `pwd`
+	popd > /dev/null
+}
+export -f cdr_exedir
+
+function cdr_colorize () {
+	echo "$1${@:2}$COL_NORM"
+}
+export -f cdr_colorize
+
+function cdr_log () {
+	exe="$(basename $0)"
+	echo "$(cdr_colorize $COL_RED [${exe%.*}]) ${@# *}"
+}
+export -f cdr_log
