30c0d4f3583560d45123c7a840a43db5ba4e3125
Robin Broda <robin@broda.me>
2016-11-27 00:03:41 +0100

    add git-cross-pick()

--- bash_include/functions.sh
+++ bash_include/functions.sh
@@ -6,3 +6,9 @@ if $(locate_package bauerbill); then
     )
   }
 fi
+
+if $(locate_package git); then
+  function git-cross-pick () {
+    git --git-dir="$1/.git" format-patch -k -1 --stdout "$2" | git am -3 -k
+  }
+fi
