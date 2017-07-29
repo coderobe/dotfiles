f8aec4c6c00b65083a97d86f85dfbf0cdab92809
Robin Broda <robin@broda.me>
2017-07-29 15:44:18 +0200

    fix: reload systemd user state after linking services

--- bash_execute/link-systemd.sh
+++ bash_execute/link-systemd.sh
@@ -5,6 +5,7 @@ mkdir -p "$SYSTEMD_USER_DIR" > /dev/null 2>&1
 	for service in *; do
 		if [ ! -f "$SYSTEMD_USER_DIR/$service" ]; then
 			ln -s $(pwd)/$service "$SYSTEMD_USER_DIR/" && cdr_log $(cdr_colorize $COL_GREEN "Linked $service")
+			systemctl --user daemon-reload
 		fi
 	done
 )
