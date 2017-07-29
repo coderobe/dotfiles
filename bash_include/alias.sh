f619a0ba36231392401f0b36a1972e37ae55161d
Robin Broda <robin@broda.me>
2017-07-29 15:46:07 +0200

    fix: allow parameter expansion in aliases, add pacman alias

--- bash_include/alias.sh
+++ bash_include/alias.sh
@@ -1,4 +1,8 @@
 alias pastebin="curl -F c=@- https://ptpb.pw"
 
-alias ls='ls --color=auto'
+alias ls='ls --color=auto '
 alias userctl='systemctl --user '
+alias pacman='sudo pacman '
+
+# allow parameter expansion
+alias sudo='sudo '
