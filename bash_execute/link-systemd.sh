ed11c6facfb781002f635c05d9599ac2d0404f86
Robin B <robin@broda.me>
2017-10-11 21:15:31 +0200

    Escape quotes properly, Change `test` to `[`

--- bash_execute/link-systemd.sh
+++ bash_execute/link-systemd.sh
@@ -1,13 +1,13 @@
 SYSTEMD_USER_DIR="$HOME/.config/systemd/user"
-if test "$(ls -A "$(cdr_exedir)/../systemd")"; then
+if [ "$(ls -A \"$(cdr_exedir)/../systemd\")" ]; then
 (
-	mkdir -p "$SYSTEMD_USER_DIR" > /dev/null 2>&1
-	cd $(cdr_exedir)/../systemd
-	for service in *; do
-		if [ ! -f "$SYSTEMD_USER_DIR/$service" ]; then
-			ln -s $(pwd)/$service "$SYSTEMD_USER_DIR/" && cdr_log $(cdr_colorize $COL_GREEN "Linked $service")
-			systemctl --user daemon-reload
-		fi
-	done
+		mkdir -p "$SYSTEMD_USER_DIR" > /dev/null 2>&1
+		cd $(cdr_exedir)/../systemd
+		for service in *; do
+			if [ ! -f "$SYSTEMD_USER_DIR/$service" ]; then
+				ln -s $(pwd)/$service "$SYSTEMD_USER_DIR/" && cdr_log $(cdr_colorize $COL_GREEN "Linked $service")
+				systemctl --user daemon-reload
+			fi
+		done
 )
 fi
