f6c1e8609388180902930ccbb7ec4b55cffec6ba
Robin B <robin@broda.me>
2017-10-11 21:18:59 +0200

    Set variable expansion to explicit

--- bash_execute/link-systemd.sh
+++ bash_execute/link-systemd.sh
@@ -1,11 +1,11 @@
 SYSTEMD_USER_DIR="$HOME/.config/systemd/user"
 if [ "$(ls -A \"$(cdr_exedir)/../systemd\")" ]; then
 (
-		mkdir -p "$SYSTEMD_USER_DIR" > /dev/null 2>&1
-		cd $(cdr_exedir)/../systemd
+		mkdir -p "${SYSTEMD_USER_DIR}" > /dev/null 2>&1
+		cd "$(cdr_exedir)/../systemd"
 		for service in *; do
-			if [ ! -f "$SYSTEMD_USER_DIR/$service" ]; then
-				ln -s $(pwd)/$service "$SYSTEMD_USER_DIR/" && cdr_log $(cdr_colorize $COL_GREEN "Linked $service")
+			if [ ! -f "${SYSTEMD_USER_DIR}/${service}" ]; then
+				ln -s "$(pwd)/${service}" "${SYSTEMD_USER_DIR}/" && cdr_log $(cdr_colorize $COL_GREEN "Linked ${service}")
 				systemctl --user daemon-reload
 			fi
 		done
