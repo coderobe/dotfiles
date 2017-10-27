919d6b669ee7ce3a346847a662887ba0d5299279
Robin Broda <robin@broda.me>
2017-10-27 02:34:27 +0200

    Add PATH deduplication, Add debug logging

--- bash_include/path.sh
+++ bash_include/path.sh
@@ -1,9 +1,17 @@
 function addToPath () {
-  export PATH=$PATH:$1
+  cdr_debug_log "Appending '$(cdr_colorize ${COL_LIGHTBLUE} $1)' to the PATH"
+  case ":${PATH}:" in
+    *":$1:"*) cdr_debug_log "Duplicate path '$(cdr_colorize ${COL_LIGHTBLUE} $1)'";;
+    *) export PATH="${PATH}:$1";;
+  esac
 }
 
 function addToPathStart () {
-  export PATH=$1:$PATH
+  cdr_debug_log "Prepending '$(cdr_colorize ${COL_LIGHTBLUE} $1)' to the PATH"
+  case ":${PATH}:" in
+    *":$1:"*) cdr_debug_log "Duplicate path '$(cdr_colorize ${COL_LIGHTBLUE} $1)'";;
+    *) export PATH="$1:${PATH}";;
+  esac
 }
 
 if $(locate_package ruby);then
@@ -12,7 +20,7 @@ fi
 
 if $(locate_package perl);then
   addToPath "/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl"
-  addToPath "$HOME/.perl5/bin"
+  addToPath "${HOME}/.perl5/bin"
 fi
 
 CDR_BINDIR="$HOME/Documents/Scripts/bin"
@@ -25,5 +33,22 @@ if $(locate_package ccache) && $(locate_package colorgcc); then
 fi
 
 if [ -d "/usr/local/vitasdk" ]; then
-  addToPathStart "$VITASDK/bin"
+  addToPathStart "${VITASDK}/bin"
 fi
+
+cdr_debug_log "Path now '$(cdr_colorize ${COL_LIGHTBLUE} ${PATH})'"
+cdr_debug_log "Cleaning up..."
+if [ -n "${PATH}" ]; then
+  old_PATH=$PATH:; PATH=
+  while [ -n "${old_PATH}" ]; do
+    x=${old_PATH%%:*}
+    case ${PATH}: in
+      *:"$x":*) ;;
+      *) PATH=$PATH:$x;;
+    esac
+    old_PATH=${old_PATH#*:}
+  done
+  PATH=${PATH#:}
+  unset old_PATH x
+fi
+cdr_debug_log "Path now '$(cdr_colorize ${COL_LIGHTBLUE} ${PATH})'"
