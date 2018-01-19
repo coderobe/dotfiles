54363a8ef9b2324372de94a89227baf60b94f72a
Robin Broda <robin@broda.me>
2018-01-19 03:06:30 +0100

    Fix indentation

--- bash_include/1_helpers.sh
+++ bash_include/1_helpers.sh
@@ -3,22 +3,22 @@ function locate_package () {
 }
 
 function cdr_exedir () {
-	pushd `dirname $0` > /dev/null
-	echo `pwd`
-	popd > /dev/null
+  pushd `dirname $0` > /dev/null
+  echo `pwd`
+  popd > /dev/null
 }
 export -f cdr_exedir
 
 function cdr_colorize () {
-	echo "$1${@:2}$COL_NORM"
+  echo "$1${@:2}$COL_NORM"
 }
 export -f cdr_colorize
 
 function cdr_log () {
-	exe="$(basename -- $0)"
+  exe="$(basename -- $0)"
   output="${exe}"
   if ! [ -z "${CDR_SCRIPT}" ]; then output="${CDR_SCRIPT}"; fi
-	echo -e "$(cdr_colorize $COL_LIGHTRED [${output%.*}]) ${@# *}"
+  echo -e "$(cdr_colorize $COL_LIGHTRED [${output%.*}]) ${@# *}"
 }
 export -f cdr_log
 
