ef52157689d5a07f41e90a945ce2ed6b2956b763
Robin Broda <robin@broda.me>
2018-10-06 12:40:23 +0200

    functions: remove bb

--- bash_include/functions.sh
+++ bash_include/functions.sh
@@ -20,15 +20,6 @@ if locate_package cpp; then
   }
 fi
 
-if locate_package bauerbill; then
-  function bb () {
-    (
-      cd ~ &&
-      bb-wrapper "$@"
-    )
-  }
-fi
-
 if locate_package git; then
   function git-cross-pick () {
     git --git-dir="$1/.git" format-patch -k -1 --stdout "$2" | git am -3 -k
